9053cd9b9bd8c7f5b8a29f99f3def330169357c4
Adjamilton Junior <jr@ieee.org>
2015-12-16 04:20:20 -0300

    Update

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+	FIRE - File Repository (v0.1.0alpha)
+	Copyright (c) 2016, Adjamilton Junior (jr@ieee.org).
+	
+	Licensed under MIT License.	
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="author" content="Adjamilton Junior">
+		<meta name="description" content="Fire is a web application for sharing files.">
+		<meta name="keywords" content="Fire, File Repository, share, file, server">
+		<title>FIRE - File Repository</title>
+		
+		<!-- Bootstrap 3.3.6 -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
+		
+		<!-- Local CSS -->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		
+		<!-- Font Awesome 4.5.0 -->
+		<link rel="stylesheet" type="text/css" href="css/font-awesome.css">	
+	</head>
+	<body>
+		<header class="container-fluid">
+			
+		</header>
+		
+		<main class="container">
+			
+		</main>
+		
+		<footer class="container-fluid text-center">
+			Made with <i class="fa fa-heart-o"></i> by Adjamilton Junior. Enjoy it! ;-)
+		</footer>
+	</bode>
+</html>
+	
